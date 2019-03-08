b381f3f0d534426e62907bb4639f0ea88bbba5d9
Lisa Lemons - Grand Circus <LisaLemonsGrandCircus@Katies-MacBook-Air.local>
2019-03-07 20:31:32 -0500

    init commit

---
+++ main.js
@@ -0,0 +1,9 @@
+$(() => {
+    // RGB Circles
+    // Add the css class 'bg-red' to the circle with the id #make-me-red
+    // Now do the same for green and blue...
+
+    // CMYK
+    // Do the same as above for CMYK circles
+    // Note that the K in CYMK means black. Check the CSS file for the appropate background classes
+});
